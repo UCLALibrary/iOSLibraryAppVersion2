c4751c0ca9c12efac6428f87d30384acdd17396b
hsuregan <hsuregan@gmail.com>
2016-05-01 22:06:43 -0700

    credits view finished - next step populate w credits

---
+++ UCLA Library/CreditsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreditsViewController.swift
+//  UCLA Library
+//
+//  Created by Regan Hsu on 4/24/16.
+//  Copyright © 2016 Regan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
